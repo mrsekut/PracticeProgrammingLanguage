70cf56687576597bfca9584c8017945e804d4a21
mrsekut <k.cloudspider@gmail.com>
2018-08-30 02:24:57 +0900

    [Add]2.6.2

--- Rust/book/02/2.6/mandelbrot/src/main.rs
+++ Rust/book/02/2.6/mandelbrot/src/main.rs
@@ -6,16 +6,48 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32>{
     let mut z = Complex {re: 0.0, im: 0.0};
     for i in 0..limit {
         z = z * z + c;
-        if z.norm_sqr() > 4.0 {
+        if z.norm_sqr() > 4.0 { // zの原点からの距離の二乗を返す
             return Some(i);
         }
     }
     None
 }
 
-/*
-memo
+use std::str::FromStr;
 
-z.norm_sqr(): zの原点からの距離の二乗を返す
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)>{
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])){
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
 
-*/
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",        ','), None);
+    assert_eq!(parse_pair::<i32>("10",      ','), None);
+    assert_eq!(parse_pair::<i32>(",10",     ','), None);
+    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<i32>("0.5x",    'x'), None);
+    assert_eq!(parse_pair::<i32>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+// 感まで分けられた浮動小数点数のペアをパースして複素数を返す
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ','){
+        Some((re, im)) => Some(Complex {re, im}),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25, -0.0625"), Some(Complex {re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
