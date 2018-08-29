68ecb80aa8f7d3188469c1a23867a85d7c6b0a54
mrsekut <k.cloudspider@gmail.com>
2018-08-30 02:07:28 +0900

    [Add]2.6.1

--- Rust/book/02/2.6/mandelbrot/src/main.rs
+++ Rust/book/02/2.6/mandelbrot/src/main.rs
@@ -2,9 +2,20 @@ extern crate num
 use num::Complex;
 
 #[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>) {
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32>{
     let mut z = Complex {re: 0.0, im: 0.0};
-    loop {
+    for i in 0..limit {
         z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
     }
+    None
 }
+
+/*
+memo
+
+z.norm_sqr(): zの原点からの距離の二乗を返す
+
+*/
