6db79cb575f61dcebe54def6da8fd1a3044494f0
mrsekut <k.cloudspider@gmail.com>
2018-09-03 00:38:18 +0900

    2018-9-3

--- Haskell/src/03.type.hs
+++ Haskell/src/03.type.hs
@@ -58,3 +58,40 @@ factorial 50
 -- Integral型クラス
 -- 整数全体のみが含まれる数の型クラス
 -- Int,Integerを含む
+
+
+-- ==============================
+--
+-- 自作 型
+--
+-- ==============================
+
+-- 型
+-----------------------------------
+
+-- Shapeは自分でつくった型
+-- 以下のCircle,Rectangleなどを`値コンストラクタ`と呼ぶ
+data Shape = Circle Float Float Float |       -- x, y, r
+             Rectangle Float Float Float Float -- 左下(x, y), 右上(x, y)
+     deriving (Show)
+
+area :: Shape -> Float
+area (Circle _ _ r)          = pi * r ^ 2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+-- 上を座標をPointという型にした
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+
+area :: Shape -> Float
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 x2) (Point y1 y2)) = (abs $ x2 - x2) * (abs $ y2 - y1)
+
+-- レコード構文
+-- 名前をつけることで、利用するときに順番を考慮しなくても良くなる
+data Person = Person { firstName:: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
