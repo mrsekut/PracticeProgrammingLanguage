d1d9cbd5029bb7dd6cad37b5f597ef0cca7bab12
mrsekut <k.cloudspider@gmail.com>
2018-09-14 00:19:49 +0900

    2018-9-14

--- Go/src/04.struct.go
+++ Go/src/04.struct.go
@@ -1,6 +1,8 @@
-package main // type
+package main
+
 import "fmt"
 
+// type
 // ============================
 // 型のエイリアスを定義する
 
@@ -11,6 +13,7 @@ func main() {
 		Strings     []string
 		AreaMap     map[string][2]float64 // 複雑な型にAreaMapというエイリアスを定義している
 		IntsChannel chan []int
+		Callback    func(i int) int // 関数型にエイリアスを定義
 	)
 
 	var n1 MyInt = 5
@@ -28,6 +31,7 @@ func main() {
 
 // struct
 // ============================
+// 複数の任意の型の値を1つにまとめたもの
 
 func main() {
 	// 構造体の定義
@@ -43,7 +47,63 @@ func main() {
 
 	// 複合リテラル
 	// 初期値を指定しつつ構造体を生成
-	pt := Point{1, 2}
+	pt := &Point{1, 2}
+	// pt := Point{X; 1, Y: 2} // 同じ
 
 	fmt.Println(pt.X, pt.Y)
 }
+
+// 入れ子の構造体はフィールド名が一意に決まる場合は、中間フィールド名を省略できる
+func main() {
+	type Feed struct {
+		Name   string
+		Amount uint
+	}
+
+	type Animal struct {
+		Name string
+		Feed
+	}
+	a := Animal{
+		Name: "monkey",
+		Feed: Feed{
+			Name:   "Banana",
+			Amount: 10,
+		},
+	}
+
+	fmt.Println(a.Amount) // ←本当はa.Feed.Amountと書かないといけないが、今回の場合は一意なので省略可
+	fmt.Println(a.Feed.Name)
+}
+
+// メソッド
+type Point struct{ X, Y int }
+
+// *Point型のメソッド「Render」を定義
+// `func`とメッソド名の間に型であるレシーバーを書く
+func (p *Point) Render() {
+	fmt.Printf("<%d, %d>\n", p.X, p.Y)
+}
+
+func main() {
+	p := &Point{X: 5, Y: 12}
+	p.Render()
+}
+
+// 型のコンストラクタ
+type User struct {
+	Id   int
+	Name string
+}
+
+// 慣例的に`new`,`New`から関数名を始める
+func NewUser(id int, name string) *User {
+	u := new(User)
+	u.Id = id
+	u.Name = name
+	return u
+}
+
+// interface
+// ============================
+//
