5bccfc3503a9041779931406521f5f3b5e1a1e2a
mrsekut <k.cloudspider@gmail.com>
2018-10-08 13:30:34 +0900

    2018-10-8

---
+++ Haskell/src/compro.hs
@@ -0,0 +1,9 @@
+-- 参考: https://qiita.com/karszawa/items/ec0c01c81c22ce060405
+-- 実行
+-- `$ runhaskell hoge.hs`
+
+-- 問題
+-- https://qiita.com/hsjoihs/items/25a08b426196ab2b9bb0
+-- http://hamukichi.hatenablog.jp/entry/2018/01/08/223316
+
+main = getLine >>= putStrLn . solve
