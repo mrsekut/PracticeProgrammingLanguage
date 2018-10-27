6a74e1cb880bc7eac140e7b539d59e64f8ed9d80
mrsekut <k.cloudspider@gmail.com>
2018-10-27 22:29:35 +0900

    [Fix]

--- Go/observer_pattern/simple.go
+++ Go/observer_pattern/simple.go
@@ -4,36 +4,32 @@ import (
 	"fmt"
 )
 
-// Subject
+// 具象クラス Subject
 // =======================
 
 type Subject struct {
 	observers []Observer
 }
 
-// constructor `New`で始める
 func NewSubject(name string) *Subject {
 	return &Subject{observers: []Observer{}}
 }
 
-// method
-func (nb *Subject) registerObserver(o Observer) {
-	nb.observers = append(nb.observers, o)
+func (s *Subject) addObserver(o Observer) {
+	s.observers = append(s.observers, o)
 }
 
-func (self *Subject) notifyObservers() {
-	for _, obserber := range self.observers {
-		obserber.notify()
+func (s Subject) notifyObservers() {
+	for _, observer := range s.observers {
+		observer.notify()
 	}
 }
 
-// Observer
+// 具象クラス Observer
 // =======================
 
-type Param string
-
 type IObserver interface {
-	notify(p Param)
+	notify()
 }
 
 type Observer struct {
@@ -45,20 +41,20 @@ func NewObserver(name string) *Observer {
 	return &Observer{name: name}
 }
 
-func (self *Observer) notify() {
-	fmt.Printf("name: %v\n", self.name)
+func (o Observer) notify() {
+	fmt.Printf("hello! i am %v\n", o.name)
 }
 
 // main
 // =======================
 
 func main() {
-	oreilly := NewSubject("oreilly")
-	john := NewObserver("john")
-	paul := NewObserver("paul")
+	s := NewSubject("sms")
+	mom := NewObserver("mom")
+	gf := NewObserver("girlFriend")
 
-	oreilly.registerObserver(*john)
-	oreilly.registerObserver(*paul)
+	s.addObserver(*mom)
+	s.addObserver(*gf)
 
-	oreilly.notifyObservers()
+	s.notifyObservers()
 }
