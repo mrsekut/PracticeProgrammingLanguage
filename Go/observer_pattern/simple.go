fae9fcc5cdc59172268c98692c697fa5137a8487
mrsekut <k.cloudspider@gmail.com>
2018-10-28 02:20:08 +0900

    [Fix]delete interface

--- Go/observer_pattern/simple.go
+++ Go/observer_pattern/simple.go
@@ -1,25 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // 具象クラス Subject
 // =======================
 
 type Subject struct {
+	name      string
 	observers []Observer
 }
 
-func NewSubject(name string) *Subject {
-	return &Subject{observers: []Observer{}}
+func newSubject(name string) *Subject {
+	return &Subject{name: name, observers: []Observer{}}
 }
 
 func (s *Subject) addObserver(o Observer) {
 	s.observers = append(s.observers, o)
 }
 
-func (s Subject) notifyObservers() {
+func (s *Subject) notifyObservers() {
 	for _, observer := range s.observers {
 		observer.notify()
 	}
@@ -28,20 +27,15 @@ func (s Subject) notifyObservers() {
 // 具象クラス Observer
 // =======================
 
-type IObserver interface {
-	notify()
-}
-
 type Observer struct {
 	name string
-	IObserver
 }
 
-func NewObserver(name string) *Observer {
+func newObserver(name string) *Observer {
 	return &Observer{name: name}
 }
 
-func (o Observer) notify() {
+func (o *Observer) notify() {
 	fmt.Printf("hello! i am %v\n", o.name)
 }
 
@@ -49,9 +43,9 @@ func (o Observer) notify() {
 // =======================
 
 func main() {
-	s := NewSubject("sms")
-	mom := NewObserver("mom")
-	gf := NewObserver("girlFriend")
+	s := newSubject("sms")
+	mom := newObserver("mom")
+	gf := newObserver("girlFriend")
 
 	s.addObserver(*mom)
 	s.addObserver(*gf)
