1db94351e6a411782e3ba45e894a7d0057cfb21d
mrsekut <k.cloudspider@gmail.com>
2018-11-04 01:31:14 +0900

    2018-11-3

---
+++ Rust/src/compro.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn main() {
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line"); // Errのときこの行が実行
+    print!("number: {:?}", guess);
+}
