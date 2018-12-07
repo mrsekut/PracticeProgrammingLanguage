6d6f517f2d2f9035309842b54d66776bc6f94d38
mrsekut <k.cloudspider@gmail.com>
2018-12-08 00:26:06 +0900

    2018-12-8

--- Nim/src/compro.nim
+++ Nim/src/compro.nim
@@ -1,36 +1,51 @@
 # 実行
-$ nim c -r prac
+# $ nim c -r prac
 
-# https://qiita.com/ishowta/items/a4d098191fda0f964c3d
+# 全体的に        https://qiita.com/ishowta/items/a4d098191fda0f964c3d
+# コレクション操作 https://qiita.com/6in/items/61d16b4b86dcd15dedc5
+
+# とりあえず全部
+# import system # systemはコンパイルエラーになる(ver 0.13.0)
+import algorithm, critbits, future, intsets, lists, macros, math, queues, sequtils, sets, strutils, tables
 
 # 入出力
-import sequtils
-from strutils import split, parseInt
-from math import sum
-let m = readLine(stdin) # 一行入力
-let n = readLine(stdin).parseInt() # 一行入力int
-let m = readLine(stdin).split(',') # 区切って入力
-let m = readLine(stdin).split().map(parseInt)  # 空白でsplitしてintに変換(seq[int])
+# ==================
+let m = stdin.readLine()
+# ex. 3\n -> 3
 
-# mapを使う時は
-import sequtils
+let n = stdin.readLine.parseInt # 一行入力int
+let m = stdin.readLine.split(',') # 区切って入力
 
-# https://qiita.com/ishowta/items/a4d098191fda0f964c3d
-# 競技プログラミングのためのNim
+let m = stdin.readLine().split().map(parseInt)
+# ex. 3 4 5\n => @[3, 4, 5]
 
-# var v = [1,2,3,4,5]
-#
-# # 以下は全部同じ出力結果になる
-# echo len(v)
-# echo v.len
-# echo len v
 
-# var numOfSomeFlower = 4
-#
-# # camelCaseでもsnake_caseでも同じ解釈になる
-# echo numOfSomeFlower
-# echo num_of_some_flower
+# count
+count("1") # 配列中の`1`の数
 
-import system, macros, algorithm, tables, sets, lists, queues, intsets, critbits, sequtils, strutils, math, future, scanf
 
-var a :int32 = 1'i32
+# map :: [a] -> [b]
+let
+  b = m.map(proc(x: string): string = $x)
+  b = m.map do (x:string) -> string : $x  # do
+
+
+# foldl :: [a] -> a
+let
+  numbers = @[5, 9, 11]
+  addition = foldl(numbers, a + b)
+
+# mapIt
+# filterIt
+
+
+echo x / y # 割り算
+echo x div y # 割り算の商
+echo x mod y # 割り算のあまり
+echo x ^ y # べき乗
+echo x and y # 論理和
+echo x or y # 論理積
+echo x xor y # 排他的論理和
+echo (not x) # ビット反転
+echo x shl y # 左シフト
+echo x shr y # 右シフト
