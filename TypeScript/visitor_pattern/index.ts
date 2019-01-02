f55a8401bbad91f417b7139baa9b6a879d7fc68f
mrsekut <k.cloudspider@gmail.com>
2019-01-02 19:31:26 +0900

    [Fix]accept type

--- TypeScript/visitor_pattern/index.ts
+++ TypeScript/visitor_pattern/index.ts
@@ -1,44 +1,44 @@
 // TypeScript
 interface Visitor<N, R> {
-  plus: (e1: Expr<N, R>, e2: Expr<N, R>) => R;
-  square: (e: Expr<N, R>) => R;
+  plus: (e1: Expr<N>, e2: Expr<N>) => R;
+  square: (e: Expr<N>) => R;
   num: (e: N) => R;
 }
 
-interface Expr<N, R> {
-  accept: (v: Visitor<N, R>) => R;
+interface Expr<N> {
+  accept: <R>(v: Visitor<N, R>) => R;
 }
 
-class Plus<N, R> implements Expr<N, R> {
-  constructor(public e1: Expr<N, R>, public e2: Expr<N, R>) {}
+class Plus<N> implements Expr<N> {
+  constructor(public e1: Expr<N>, public e2: Expr<N>) {}
 
-  accept(v: Visitor<N, R>) {
+  accept<R>(v: Visitor<N, R>) {
     return v.plus(this.e1, this.e2);
   }
 }
 
-class Square<N, R> implements Expr<N, R> {
-  constructor(public e: Expr<N, R>) {}
+class Square<N> implements Expr<N> {
+  constructor(public e: Expr<N>) {}
 
-  accept(v: Visitor<N, R>) {
+  accept<R>(v: Visitor<N, R>) {
     return v.square(this.e);
   }
 }
 
-class Num<N, R> implements Expr<N, R> {
+class Num<N> implements Expr<N> {
   constructor(public n: N) {}
 
-  accept(v: Visitor<N, R>): R {
+  accept<R>(v: Visitor<N, R>) {
     return v.num(this.n);
   }
 }
 
 class Eval implements Visitor<number, number> {
-  plus(e1: Expr<number, number>, e2: Expr<number, number>): number {
+  plus(e1: Expr<number>, e2: Expr<number>): number {
     return e1.accept(this) + e2.accept(this);
   }
 
-  square(e: Expr<number, number>): number {
+  square(e: Expr<number>): number {
     let x = e.accept(this);
     return x * x;
   }
@@ -49,11 +49,11 @@ class Eval implements Visitor<number, number> {
 }
 
 class Show implements Visitor<number, string> {
-  plus(e1: Expr<number, string>, e2: Expr<number, string>): string {
+  plus(e1: Expr<number>, e2: Expr<number>): string {
     return `${e1.accept(this)} + ${e2.accept(this)}`;
   }
 
-  square(e: Expr<number, string>): string {
+  square(e: Expr<number>): string {
     return `(${e.accept(this)})^2`;
   }
 
