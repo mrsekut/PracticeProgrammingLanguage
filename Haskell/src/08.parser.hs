70552377fe6b83b994dda5e9b5ebbf4c00bc175d
mrsekut <k.cloudspider@gmail.com>
2019-03-21 22:58:03 +0900

    2019-3-21

--- Haskell/src/08.parser.hs
+++ Haskell/src/08.parser.hs
@@ -1,38 +1,55 @@
 -- ref: https://qiita.com/7shi/items/b8c741e78a96ea2c10fe
-import Control.Exception
-import Data.Char
 
-parseTest p s = do
-    print $ fst $ p s
-    `catch` \(SomeException e) ->
-        putStr $ show e
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.State
+import           Control.Applicative            ( (<$>)
+                                                , (<*>)
+                                                )
+import           Data.Char
 
--- 文字列から先頭と残りを取得
-anyChar (x:xs) = (x, xs)
 
--- 条件が指定できる版 anyChar
-satisfy f (x:xs) | f x = (x, xs)
+parseTest p s =
+    do
+            print $ fst $ p s
+        `catch` \(SomeException e) -> putStr $ show e
+
+anyChar (x : xs) = (x, xs)
+satisfy f (x : xs) | f x = (x, xs)
 
 char c = satisfy (== c)
 digit = satisfy isDigit
 letter = satisfy isLetter
 
-test3 x0 =
-    let (x1, xs1) = letter x0
+test1 xs0 =
+    let (x1, xs1) = anyChar xs0
+        (x2, xs2) = anyChar xs1
+    in  ([x1, x2], xs2)
+
+test2 xs0 =
+    let (x1, xs1) = test1 xs0
+        (x2, xs2) = anyChar xs1
+    in  (x1 ++ [x2], xs2)
+
+test3 xs0 =
+    let (x1, xs1) = letter xs0
         (x2, xs2) = digit xs1
         (x3, xs3) = digit xs2
-    in ([x1, x2, x3], xs3)
-
+    in  ([x1, x2, x3], xs3)
 
 main = do
-    -- parseTest (char 'a') "abc"
-    -- parseTest (char 'a') "123"
-    -- parseTest digit "abc"
-    -- parseTest digit "123"
-    -- parseTest letter "abc"
-    -- parseTest letter "123"
-
-    parseTest test3 "abc"
-    parseTest test3 "123"
-    parseTest test3 "a12"
-    parseTest test3 "a12oo"
+    parseTest anyChar    "abc"
+    parseTest test1      "abc"
+    parseTest test2      "abc"
+    parseTest test2      "12"      -- NG
+    parseTest test2      "123"
+    parseTest (char 'a') "abc"
+    parseTest (char 'a') "123"  -- NG
+    parseTest digit      "abc"      -- NG
+    parseTest digit      "123"
+    parseTest letter     "abc"
+    parseTest letter     "123"      -- NG
+    parseTest test3      "abc"      -- NG
+    parseTest test3      "123"      -- NG
+    parseTest test3      "a23"
+    parseTest test3      "a234"
