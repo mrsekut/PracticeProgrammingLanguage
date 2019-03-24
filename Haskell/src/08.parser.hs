af1240b9952f7553123a69a8be5ec9f136031f3d
mrsekut <k.cloudspider@gmail.com>
2019-03-24 20:04:08 +0900

    [Add]parssec

--- Haskell/src/08.parser.hs
+++ Haskell/src/08.parser.hs
@@ -1,50 +1,18 @@
 -- ref: https://qiita.com/7shi/items/b8c741e78a96ea2c10fe
 
-import           Control.Exception
-import           Control.Monad
-import           Control.Monad.State
+import           Text.Parsec
 import           Control.Applicative            ( (<$>)
                                                 , (<*>)
                                                 )
-import           Data.Char
 
-
-parseTest p s = case p s of
-    Right (r, _) -> print r
-    Left  e      -> putStrLn $ "[" ++ show s ++ "]" ++ e
-
-
-anyChar (x : xs) = Right (x, xs)
-anyChar _        = Left "too short"
-
-satisfy f (x : xs) | not $ f x = Left $ " : " ++ show x
-satisfy f xs                   = anyChar xs
-
-Left a <|> Left b = Left $ b ++ a
-Left _ <|> b      = b
-a      <|> _      = a
-
-char c xs = satisfy (== c) xs <|> Left ("not char" ++ show c)
-digit xs = satisfy isDigit xs <|> Left ("not digit")
-letter xs = satisfy isLetter xs <|> Left "not letter"
-
-
-test1 xs0 = do
-    (x1, xs1) <- anyChar xs0
-    (x2, xs2) <- anyChar xs1
-    return ([x1, x2], xs2)
-
-test2 xs0 = do
-    (x1, xs1) <- test1 xs0
-    (x2, xs2) <- anyChar xs1
-    return (x1 ++ [x2], xs2)
-
-test3 xs0 = do
-    (x1, xs1) <- letter xs0
-    (x2, xs2) <- digit xs1
-    (x3, xs3) <- digit xs2
-    return ([x1, x2, x3], xs3)
-
+test1 = sequence [anyChar, anyChar]
+test2 = (++) <$> test1 <*> sequence [anyChar]
+test3 = sequence [letter, digit, digit]
+test4 = letter <|> digit
+test5 = sequence [letter, digit, digit, digit]
+test6 = sequence $ letter : replicate 3 digit
+test7 = many letter
+test8 = many (letter <|> digit)
 
 main = do
     parseTest anyChar    "abc"
@@ -62,3 +30,14 @@ main = do
     parseTest test3      "123"      -- NG
     parseTest test3      "a23"
     parseTest test3      "a234"
+    parseTest test4      "a"
+    parseTest test4      "1"
+    parseTest test4      "!"        -- NG
+    parseTest test5      "a123"
+    parseTest test5      "ab123"    -- NG
+    parseTest test6      "a123"
+    parseTest test6      "ab123"    -- NG
+    parseTest test7      "abc123"
+    parseTest test7      "123abc"
+    parseTest test8      "abc123"
+    parseTest test8      "123abc"
