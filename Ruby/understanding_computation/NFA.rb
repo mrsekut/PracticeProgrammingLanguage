184658f04b74f252b82341874f8ff627a9c5f432
mrsekut <k.cloudspider@gmail.com>
2019-04-15 20:16:37 +0900

    2019-4-15

--- Ruby/understanding_computation/NFA.rb
+++ Ruby/understanding_computation/NFA.rb
@@ -56,7 +56,7 @@ class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
         to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
     end
 
-    def to_nfa
+def to_nfa
         NFA.new(Set[start_state], accept_states, rulebook)
     end
 end
