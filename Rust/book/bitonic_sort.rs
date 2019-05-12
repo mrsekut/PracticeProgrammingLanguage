7e84cc3830c0e9c7066353ef99be6b022390293f
mrsekut <k.cloudspider@gmail.com>
2019-05-13 01:47:17 +0900

    2019-5-13

--- Rust/book/bitonic_sort.rs
+++ Rust/book/bitonic_sort.rs
@@ -1,131 +1,119 @@
-// mod tests {
-//     struct Student {
-//         first_name: String,
-//         last_name: String,
-//         age: u8,
-//     }
-
-//     impl Student {
-//         fn new(first_name: &str, last_name: &str, age: u8) -> Self {
-//             Self {
-//                 first_name: first_name.to_string(),
-//                 last_name: last_name.to_string(),
-//                 age,
-//             }
-//         }
-//     }
-
-//     use super::{is_power_of_two, sort, sort_by};
-//     use crate::SortOrder::*;
-
-//     #[test]
-//     fn sort_students_by_age_ascending() {
-//         let taro = Student::new("Taro", "Yamada", 16);
-//         let hanako = Student::new("Hanako", "Yamada", 14);
-//         let kyoko = Student::new("Kyoko", "Ito", 15);
-//         let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
-
-//         let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
-//         let expected = vec![&hanako, &kyoko, &taro, &ryosuke];
-
-//         assert_eq!(
-//             sort_by(&mut x, &|a, b| a.age.cmp(&b.age)),
-//             Ok(())
-//         );
-
-//         assert_eq!(x, expected);
-//     }
-
-// }
-
 use rand::{Rng, SeedableRng};
 use rand::distributions::Standard;
 use rand_pcg::Pcg64Mcg;
+use rayon;
 
-fn new_u32_vec(n: usize) -> Vec<u32> {
-    let mut rng = Pcg64Mcg::from_seed([0; 16]);
-    rng.sample_iter(&Standard).take(n).collect()
-}
-
-fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
-    x.windows(2).all(|pair| pair[0] <= pair[1])
-}
-
-fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
-    x.windows(2).all(|pair| pair[0] >= pair[1])
-}
-
-
-
-
+use std::cmp::Ordering;
 
 enum SortOrder {
     Ascending,
     Descending,
 }
 
-fn do_sort<T: Ord>(x: &mut [T], up: bool) {
-    if x.len() > 1 {
-        let mid_point = x.len() / 2;
-        do_sort(&mut x[..mid_point], true);
-        do_sort(&mut x[mid_point..], false);
-        sub_sort(x, up);
+const PARALLEL_THRESHOLD: usize = 4096;
+
+fn sort<T: Ord + Send>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    match *order {
+        SortOrder::Ascending  => sort_by(x, &|a, b| a.cmp(b)),
+        SortOrder::Descending => sort_by(x, &|a, b| b.cmp(a)),
     }
 }
 
-fn sort<T: Ord>(x: &mut [T], order: &SortOrder ) -> Result<(), String> {
+fn sort_by<T, F>(x: &mut [T], comparator: &F) -> Result<(), String>
+    where T: Send,
+          F: Sync + Fn(&T, &T) -> Ordering
+{
     if x.len().is_power_of_two() {
-        match *order {
-            SortOrder::Ascending => do_sort(x, true),
-            SortOrder::Descending => do_sort(x, false),
-        };
+        do_sort(x, true, comparator);
         Ok(())
     } else {
         Err(format!("The length of x is not a power of two. (x.len(): {})", x.len()))
     }
 }
 
-fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
+fn do_sort<T, F>(x: &mut [T], forward: bool, comparator: &F)
+    where T: Send,
+          F: Sync + Fn(&T, &T) -> Ordering
+{
     if x.len() > 1 {
-        comare_and_swap(x, up);
-        let mid_point = x.len() /2;
-        sub_sort(&mut x[..mid_point], up);
-        sub_sort(&mut x[mid_point..], up);
+        let mid_point = x.len() / 2;
+        let (first, second) = x.split_at_mut(mid_point);
+        if mid_point >= PARALLEL_THRESHOLD {
+            rayon::join(|| do_sort(first, true, comparator),
+                        || do_sort(second, false, comparator));
+        } else {
+            do_sort(first, true, comparator);
+            do_sort(second, false, comparator);
+        }
+        sub_sort(x, forward, comparator);
     }
 }
 
-fn comare_and_swap<T: Ord>(x: &mut [T], up: bool) {
-    let mid_point = x.len() / 2;
-    for i in 0..mid_point {
-        if (x[i] > x[mid_point + i]) == up {
-            x.swap(i, mid_point + i);
+fn sub_sort<T, F>(x: &mut [T], forward: bool, comparator: &F)
+    where T: Send,
+          F: Sync + Fn(&T, &T) -> Ordering
+{
+    if x.len() > 1 {
+        compare_and_swap(x, forward, comparator);
+        let mid_point = x.len() / 2;
+        let (first, second) = x.split_at_mut(mid_point);
+        if mid_point >= PARALLEL_THRESHOLD {
+            rayon::join(|| sub_sort(first, forward, comparator),
+                        || sub_sort(second, forward, comparator));
+        } else {
+            sub_sort(first, forward, comparator);
+            sub_sort(second, forward, comparator);
         }
     }
 }
 
+
+fn compare_and_swap<T, F>(x: &mut [T], forward: bool, comparator: &F)
+    where F: Fn(&T, &T) -> Ordering
+{
+    let swap_condition = if forward {
+        Ordering::Greater
+    } else {
+        Ordering::Less
+    };
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if comparator(&x[i], &x[mid_point + i]) == swap_condition {
+            x.swap(i, mid_point + i);
+        }
+    }
+}
 fn main() {
     unimplemented!();
 }
 
+
 #[cfg(test)]
-mod tests{
-    use super::sort;
+mod tests {
+    use super::{sort, sort_by};
     use crate::SortOrder::*;
-    use super::{ new_u32_vec, is_sorted_ascending, is_sorted_descending };
 
+    #[derive(Debug, PartialEq)]
+    struct Student {
+        first_name: String,
+        last_name:  String,
+        age: u8,
+    }
 
-    #[test]
-    fn sort_u32_ascending() {
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        assert_eq!(sort(&mut x, &Ascending), Ok(()));
-        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    impl Student {
+        fn new(first_name: &str, last_name: &str, age: u8) -> Self {
+            Self {
+                first_name: first_name.to_string(),
+                last_name:  last_name.to_string(),
+                age,
+            }
+        }
     }
 
     #[test]
-    fn sort_u32_descending() {
-        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        assert_eq!(sort(&mut x, &Descending), Ok(()));
-        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    fn sort_to_fail() {
+        let mut x = vec![10, 30, 11]; // x.len() が2のべき乗になっていない。
+        assert!(sort(&mut x, &Ascending).is_err());
     }
 
     #[test]
@@ -143,23 +131,35 @@ mod tests{
     }
 
     #[test]
-    fn sort_to_fail() {
-        let mut x = vec![10, 30, 11];
-        assert!(sort(&mut x, &Ascending).is_err());
+    fn sort_students_by_age_ascending() {
+        let taro = Student::new("Taro", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("Kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+        let expected = vec![&hanako, &kyoko, &taro, &ryosuke];
+        assert_eq!(
+            sort_by(&mut x, &|a, b| a.age.cmp(&b.age)),
+            Ok(())
+        );
+        assert_eq!(x, expected);
     }
 
     #[test]
-    fn sort_u32_large() {
-        {
-            let mut x = new_u32_vec(65536);
-            assert_eq!(sort(&mut x, &Ascending), Ok(()));
-            assert!(is_sorted_ascending(&x));
-        }
-        {
-            let mut x = new_u32_vec(65536);
-            assert_eq!(sort(&mut x, &Descending), Ok(()) );
-            assert!(is_sorted_descending(&x));
-        }
-    }
+    fn sort_students_by_name_ascending() {
+        let taro = Student::new("Taro", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("Kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
 
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+        let expected = vec![&ryosuke, &kyoko, &hanako, &taro];
+
+        assert_eq!(sort_by(&mut x,
+            &|a, b| a.last_name.cmp(&b.last_name)
+                .then_with(|| a.first_name.cmp(&b.first_name))), Ok(())
+        );
+        assert_eq!(x, expected);
+    }
 }
