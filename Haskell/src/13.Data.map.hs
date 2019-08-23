9861324d6c652816bad48865c737cc3e1cd3655b
mrsekut <k.cloudspider@gmail.com>
2019-08-23 23:13:57 +0900

    2019-8-23

---
+++ Haskell/src/13.Data.map.hs
@@ -0,0 +1,17 @@
+import qualified Data.Map                      as Map
+
+env :: Map.Map String String
+env = Map.fromList [("hoge", "42"), ("piyo", "100")] -- 初期化
+
+main = do
+    -- lookup :: Ord k => k -> Map.Map k a -> Maybe a
+    print $ Map.lookup "hoge" env   -- 値がある時
+
+    -- insert :: Ord k => k -> a -> Map.Map k a -> Map.Map k a
+    let env2 = Map.insert "foo" "500" env   -- envに新しくkey-valueを追加したもんをおenv2に束縛
+    print env2
+    print $ Map.lookup "foo" env2
+
+    -- delete :: Ord k => k -> Map.Map k a -> Map.Map k a
+    let env3 = Map.delete "hoge" env        -- "hoge"を削除したものをenv3に束縛
+    print $ Map.lookup "hoge" env3          --値がない時
